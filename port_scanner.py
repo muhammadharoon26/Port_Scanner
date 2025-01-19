95837bde08105927c72ee54ccc88411c113c154b
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-19 23:29:57 +0500

    `Refactored port scanner script to improve structure and readability`

--- port_scanner.py
+++ port_scanner.py
@@ -1,38 +1,54 @@
 #!/bin/python3
-import sys #allows us to enter command line arguments among other things
+import sys
 import socket
 from datetime import datetime
-#define our target
-if(len(sys.argv)==2):
-        target = socket.gethostbyname(sys.argv[1])
-else:
-        print("invalid amount of arguments")
-        print(" python3 port_scanner.py <ip>")
+
+
+def get_target():
+    if len(sys.argv) == 2:
+        try:
+            return socket.gethostbyname(sys.argv[1])
+        except socket.gaierror:
+            print("Hostname could not be resolved")
+            sys.exit()
+    else:
+        print("Invalid amount of arguments")
+        print("python3 port_scanner.py <ip>")
         sys.exit()
-#add pretty banner
-print("-"*50)
-print("scanning target" + target)
-print("time started:" + str(datetime.now()))
-print("-"*50)
-try:
-        for port in range(50,86):
-            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1) #is a float
-            result = s.connect_ex((target,port)) #returns error indicator
-            print("checking port {}........................................................".format(port))
-            if(result==0):
-                    print("port {} is open".format(port))
-            else:
-                    print("port {} is closed".format(port))
-            s.close()
-except KeyboardInterrupt:
-        print("-"*50)
-        print("exiting program" + "."*50)
-        print("-"*50)
+
+
+def scan_port(target, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} is open")
+        else:
+            print(f"Port {port} is closed")
+        s.close()
+    except socket.error:
+        print(f"Could not connect to port {port}")
+
+
+def main():
+    target = get_target()
+    print("-" * 50)
+    print(f"Scanning target {target}")
+    print(f"Time started: {datetime.now()}")
+    print("-" * 50)
+    try:
+        for port in range(50, 86):
+            print(
+                f"Checking port {port}........................................................"
+            )
+            scan_port(target, port)
+    except KeyboardInterrupt:
+        print("-" * 50)
+        print("Exiting program" + "." * 50)
+        print("-" * 50)
         sys.exit()
-except socket.gaierror:
-        print("hostname could not be resolved")
-        sys.exit()
-except socket.error:
-        print("could not connect to server")
-        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
